a784741a0b82740510ea02c6e87645e06b5fdef3
Christwide Oscar <triple101.games@gmail.com>
2022-02-15 19:00:00 -0600

    Strech

--- data/seeds/01-cars.js
+++ data/seeds/01-cars.js
@@ -1,1 +1,9 @@
 // STRETCH
+exports.seed = async function(knex) {
+    // Deletes ALL existing entries
+    await knex('cars').truncate();
+    return knex('frucarsits').insert([
+      {vin: 'J15E45', make: 'Toyota', model: '4Runner', mileage: 200000, title: 'Clean', transmission: 'Automatic'},
+      {vin: 'A78ET6', make: 'Toyota', model: 'Sequoia', mileage: 200000, title: 'Clean', transmission: 'Automatic'},
+    ]);
+  };
