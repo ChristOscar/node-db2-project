41dd327316f0ef9ec4c508719c2fe7b3a33ae1ba
Christwide Oscar <triple101.games@gmail.com>
2022-02-15 18:36:34 -0600

    Cars-router completed, with error handling

--- api/cars/cars-router.js
+++ api/cars/cars-router.js
@@ -1,1 +1,38 @@
-// DO YOUR MAGIC
+const Cars = require('./cars-model');
+const router = require('express').Router();
+const { checkCarId, checkCarPayload, checkVinNumberValid, checkVinNumberUnique } = require('./cars-middleware');
+
+router.get('/', (req, res, next) =>{
+    Cars.getAll()
+    .then(cars => {
+        res.json(cars);
+    })
+    .catch(next)
+});
+
+router.post('/',checkCarPayload, checkVinNumberValid, checkVinNumberUnique, (req,res, next) =>{
+    Cars.create()
+    .then(cars => {
+        res.status(201).json(cars);
+    })
+    .catch(next)
+});
+
+router.get('/:id', checkCarId, (req, res, next)=>{
+    const {id} = req.params;
+    Cars.getById(id)
+    .then(cars =>{
+        res.json(cars);
+    })
+    .catch(next)
+});
+
+router.use((err, req, res, next)=>{
+    res.status(err.status || 500).json({
+        custom: 'Something happened in the cars-router',
+        message: err.message,
+        stack: err.stack,
+    })
+})
+
+module.exports = {router};
